a5d3948d07c87c80e1a1e79cee92efed42196894
Pranav M <pranav.mappoli@walmart.com>
2024-12-01 13:31:35 +0530

    accordian

---
+++ React js template/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container flex justify-center items-center w-screen h-screen">
+      App
+    </div>
+  );
+}
+
+export default App;
