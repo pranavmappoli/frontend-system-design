269e1b626941973feca105737265777c74bcd1fa
Pranav M <pranav.mappoli@walmart.com>
2024-12-03 14:16:10 +0530

    set react template

---
+++ react+tailwind+template/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100 ">
+      app
+    </div>
+  );
+}
+
+export default App;
