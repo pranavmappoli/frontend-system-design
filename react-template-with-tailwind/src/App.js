bca609e0419c7003439f68966667ff999cc2a78c
Pranav M <pranav.mappoli@walmart.com>
2025-03-08 15:56:34 +0530

    rephase template

--- react-template-with-tailwind/src/App.js
+++ react-template-with-tailwind/src/App.js
@@ -4,7 +4,7 @@ import Calculator from "./Calculator";
 const App = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen p-8 bg-blue-300">
-      <Calculator />
+      hellooo
     </div>
   );
 };
